64eaa9f768514c97e5d9e403697308ca6c1b5d77
Mihail Lytov <m.lytov@click-group.ru>
2024-09-25 00:21:15 +0300

    refactor code

--- src/main/java/com/example/mihail/lytov/service/camunda/CamundaServiceImpl.java
+++ src/main/java/com/example/mihail/lytov/service/camunda/CamundaServiceImpl.java
@@ -27,15 +27,13 @@ public class CamundaServiceImpl implements CamundaService {
         try {
             File file = new File("src/main/resources/processes/customer_evaluation.dmn");
             DmnModelInstance modelInstance = Dmn.readModelFromFile(file);
-            //DecisionDefinition decisionDefinition = (DecisionDefinition) modelInstance.getDefinitions();
+
             Map<String, Object> inputData = new HashMap<>();
             inputData.put("region_input", dto.getRegion());
             inputData.put("capital_input", dto.getCapital());
             inputData.put("inn_input", dto.getInn().length() == 12);
             inputData.put("resident_input", dto.getResident().startsWith("9909"));
-            //inputData.put("bet2", "ножницы");
 
-            //decisionService.evaluateDecisionById();
             DmnDecisionRequirementsGraph dmnDecisionGraph = dmnEngine.parseDecisionRequirementsGraph(modelInstance);
             DmnDecision dmnDecision = dmnDecisionGraph.getDecision("result_map");
             DmnDecisionResult decisionResult = dmnEngine.evaluateDecision(dmnDecision, inputData);
