50d5e67086552ca9ff7e00a897a3e5b9ad358e77
Mihail Lytov <m.lytov@click-group.ru>
2024-09-25 04:19:39 +0300

    take dmn from camunda

--- src/main/java/com/example/mihail/lytov/service/camunda/CamundaServiceImpl.java
+++ src/main/java/com/example/mihail/lytov/service/camunda/CamundaServiceImpl.java
@@ -3,16 +3,11 @@ package com.example.mihail.lytov.service.camunda;
 import com.example.mihail.lytov.controller.dto.CustomerEvaluationDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.camunda.bpm.dmn.engine.DmnDecision;
-import org.camunda.bpm.dmn.engine.DmnDecisionRequirementsGraph;
-import org.camunda.bpm.dmn.engine.DmnDecisionResult;
-import org.camunda.bpm.dmn.engine.DmnEngine;
-import org.camunda.bpm.model.dmn.Dmn;
-import org.camunda.bpm.model.dmn.DmnModelInstance;
-import org.springframework.core.io.ClassPathResource;
+import org.camunda.bpm.dmn.engine.*;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ProcessEngine;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,21 +19,19 @@ public class CamundaServiceImpl implements CamundaService {
     private final DmnEngine dmnEngine;
 
 
+    private final ProcessEngine processEngine;
+
     public Map<String, Object> verificationUser(CustomerEvaluationDTO dto) {
         try {
-            ClassPathResource resource = new ClassPathResource("customer_evaluation.dmn");
-            File file = resource.getFile();
-            DmnModelInstance modelInstance = Dmn.readModelFromFile(file);
-
             Map<String, Object> inputData = new HashMap<>();
             inputData.put("region_input", dto.getRegion());
             inputData.put("capital_input", dto.getCapital());
             inputData.put("inn_input", !(dto.getInn().length() == 12));
             inputData.put("resident_input", !dto.getResident().startsWith("9909"));
 
-            DmnDecisionRequirementsGraph dmnDecisionGraph = dmnEngine.parseDecisionRequirementsGraph(modelInstance);
-            DmnDecision dmnDecision = dmnDecisionGraph.getDecision("result_map");
-            DmnDecisionResult decisionResult = dmnEngine.evaluateDecision(dmnDecision, inputData);
+            DecisionService decisionService = processEngine.getDecisionService();
+
+            DmnDecisionTableResult decisionResult = decisionService.evaluateDecisionTableByKey("result_map", inputData);
             return decisionResult.getFirstResult().getEntryMap();
 
         }catch (Exception e) {
